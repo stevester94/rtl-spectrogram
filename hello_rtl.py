d12b111b70f58e9b0492ff865c3e90108a598305
Steven Mackey <stevester94@gmail.com>
2022-12-29 18:08:01 -0800

    Spectrogram!

--- hello_rtl.py
+++ hello_rtl.py
@@ -6,24 +6,31 @@ from matplotlib.animation import FuncAnimation
 from threading import Thread
 from scipy.fft import fft, fftfreq, fftshift
 import numpy as np
-
+import math
 
 
 sdr = RtlSdr()
 
 # configure device
-sdr.sample_rate = 2.4e6
+sdr.sample_rate = 3.2e6
 sdr.center_freq = 104.7e6
 sdr.gain = 'auto'
 
+A = np.zeros((100, 8192))
+
+fullscale = math.sqrt(2**8 + 2**8)
+
+colorbar = None
 
 def get_samples_and_plot(_):
-    # use matplotlib to estimate and plot the PSD
-    ax.cla()
-    n_samples = 1024*256
-    samples = sdr.read_samples(n_samples)
+    global A
+    global colorbar
 
-    duration_secs = len(samples) / sdr.sample_rate
+    psd_ax.cla()
+    spectrogram_ax.cla()
+
+    n_samples = 8192
+    samples = sdr.read_samples(n_samples)
 
     N = len(samples)
     T = 1/sdr.sample_rate
@@ -34,13 +41,40 @@ def get_samples_and_plot(_):
     yplot = fftshift(yf) # Have to shift the actual fft values too
     yplot = 1.0/N * np.abs(yplot) # Normalize the magnitude of FFT
 
+    # Put in terms of dBFS
+    yplot = 10*np.log10(yplot/fullscale)
+
+    A = np.roll(A, 1, axis = 0)
+    A[0] = yplot
+    # i += 1
+    # A = np.roll(A, 1)s
+
+    # A = np.concatenate([yplot, A[:, -1]])
+
     # Fs and Fc are divided by 1e6 to make the legends nicer, since they are both divided by the same factor this doesn't mess any math up
     # NFFT splits samples into 1024 "segments", idk what this really means though
     # ax.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
     
-    ax.plot(xf+sdr.center_freq, yplot)
-    ax.set_xlabel('Frequency (MHz)')
-    ax.set_ylabel('Relative power (dB)')
+
+    x_axis = (sdr.center_freq - sdr.sample_rate/2, sdr.center_freq + sdr.sample_rate/2)
+    psd_ax.plot(xf+sdr.center_freq, yplot)
+    psd_ax.set_xlabel('Frequency (MHz)')
+    psd_ax.set_ylabel('dBFS')
+    psd_ax.set_ylim(-70, 0)
+    psd_ax.set_xlim(x_axis[0], x_axis[1])
+
+    y_axis = (100, 0)
+    pos = spectrogram_ax.imshow(A, cmap='hot', interpolation='none',  aspect='auto', extent=[x_axis[0], x_axis[1], y_axis[0], y_axis[1]],
+        vmin=-70, vmax=0)
+    spectrogram_ax.set_xlabel('Frequency (MHz)')
+    spectrogram_ax.set_ylabel('Time')
+    if colorbar is None:
+        # colorbar = fig.colorbar(pos, ax=spectrogram_ax)
+        pass
+
+
+
+
 
 def get_center_freq():
     global sdr
@@ -54,7 +88,9 @@ cli = Thread(target=get_center_freq)
 cli.start()
 
 fig = plt.figure(figsize=(12,6), facecolor='#DEDEDE')
-ax = plt.subplot(111)
+psd_ax = plt.subplot(211)
+spectrogram_ax = plt.subplot(212)
+
 ani = FuncAnimation(fig, get_samples_and_plot, interval=1)
 plt.show()
 
