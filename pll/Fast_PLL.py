778820285dfd57e53b4e6e61ae4806c4b398c49c
Steven Mackey <stevester94@gmail.com>
2023-08-22 22:48:34 -0700

    Add freq multplier to PLL (it appears to work)

--- pll/Fast_PLL.py
+++ pll/Fast_PLL.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class Fast_PhaseLockedLoop:
-    def __init__( self, K_i, K_p, K_0, pll_f, fs ) -> None:
+    def __init__( self, K_i, K_p, K_0, pll_f, fs, f_multiplier=1 ) -> None:
         self.lastSinOut = 0
 
         self.fs = fs
+        self.f_multiplier = f_multiplier
 
         # LF State
         self.K_p = K_p
@@ -15,6 +16,7 @@ class Fast_PhaseLockedLoop:
         self.K_0 = K_0
         self.nco_f = pll_f
         self.nco_theta = 0
+        self.nco_theta_multi = 0
         self.nco_phaseEstimate = 0
 
     def proc( self, in_, ):
@@ -27,10 +29,11 @@ class Fast_PhaseLockedLoop:
 
         # NCO
         self.nco_theta += 2*np.pi*self.nco_f/self.fs + self.K_0 * e_F
+        self.nco_theta_multi += self.f_multiplier * (2*np.pi*self.nco_f/self.fs + self.K_0 * e_F)
         if self.nco_theta > 2*np.pi:
             self.nco_theta -= 2*np.pi
 
-        cosOut = np.cos( self.nco_theta  )
+        cosOut = np.cos( self.nco_theta_multi  )
         sinOut = -np.sin( self.nco_theta  )
 
         self.lastSinOut = sinOut
